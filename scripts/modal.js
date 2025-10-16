925dc331e40e1364639d4bdb20f801deb43b2efa
Armen <mra06@bk.ru>
2025-10-16 19:21:30 +0300

    upd-2

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,12 +1,12 @@
-document.addEventListener('click', function(e){
-  if(e.target.matches('.open')){
+document.addEventListener('click', function (e) {
+  if (e.target.matches('.open')) {
     const id = e.target.dataset.project;
     const modal = document.getElementById('modal');
     const content = document.getElementById('modalContent');
-    content.innerHTML = '<h3 style="color:var(--accent)">Детали проекта '+id+'</h3><p>Описание проекта №'+id+'</p>';
-    modal.setAttribute('aria-hidden','false');
+    content.innerHTML = '<h3 style="color:var(--accent)">Детали проекта ' + id + '</h3><p>Описание проекта №' + id + '</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, tenetur totam qui earum beatae distinctio alias maiores, inventore at aspernatur sint non quos illum atque? Laborum non officia voluptatem tempora.</p>';
+    modal.setAttribute('aria-hidden', 'false');
   }
 });
-document.addEventListener('click', function(e){
-  if(e.target.matches('#closeModal')) document.getElementById('modal').setAttribute('aria-hidden','true');
+document.addEventListener('click', function (e) {
+  if (e.target.matches('#closeModal')) document.getElementById('modal').setAttribute('aria-hidden', 'true');
 });
